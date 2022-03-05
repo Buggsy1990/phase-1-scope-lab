398a40094c9d486a0ad9db73d40cc240a6a478f0
Christina <buggsy1106@gmail.com>
2022-03-04 21:13:01 -0600

    commit tree f92d1df2cdc63ba04d40ad40ecbd2fa716952829

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+var setBestCustomer = function(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'Bella'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Hank'
+}
